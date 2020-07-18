b8bff0c222c44b72a6a47a1c5a7afc2e7e4683b9
norrico31 <geraldbiasonjob31.com>
2020-07-18 23:52:45 +0800

    express-validator in users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'))
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password is required with 6 or more characters').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+    console.log(req.body)
+    res.send('User Route')
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const connectDB = require('./config/db')
 // Connect Database
 connectDB()
 
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => {
     res.send('Hello from express')
 })
