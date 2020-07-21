5bc277a942447a3511b4a1640f99984dae30ee92
norrico31 <geraldbiasonjob31.com>
2020-07-21 15:13:20 +0800

    User authenticate from jsonwebtoken and bcrypt hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
+
 const auth = require('../middleware/auth')
 const User = require('../models/User')
 
@@ -15,5 +20,42 @@ router.get('/', auth, async (req, res) => {
         return res.status(400).send('Server Error')
     }
 })
+
+// @route   POST api/auth
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/', [
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+    const { email, password } = req.body
+
+    try {
+        // See if the user exists
+        let user = await User.findOne({ email })
+        if (!user) return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
+
+        // 1st argument is password from req.body and 2nd parameter is from user model from database(user.password)
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] })
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+            if (err) throw err
+            res.json({ token })
+        })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 
 module.exports = router
