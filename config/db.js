cc5704244e75acaf08bdbda20cccccae0822183d
norrico31 <geraldbiasonjob31.com>
2020-07-22 15:58:31 +0800

    Create and Update a profile for a user in api/profile routes and normalize used in website props and socialfields props

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get('mongoURI')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+        await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
         console.log('MongoDB Connected')
     } catch (err) {
         console.error(err.message)
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const normalize = require('normalize-url')
 const auth = require('../middleware/auth')
+const { check, validationResult } = require('express-validator')
 
 const Profile = require('../models/Profile')
 
@@ -12,6 +14,62 @@ router.get('/me', auth, async (req, res) => {
         const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
         if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' })
         res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post('/', [ auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body
+
+    // Build profile object
+    const profileFields = {
+        user: req.user.id,
+        company,
+        location,
+        website: website && website !== '' ? normalize(website, { forceHttps: true }) : '',
+        bio,
+        skills: Array.isArray(skills) ? skills : skills.split(',').map(skill => ' ' + skill.trim()),
+        status,
+        githubusername
+    }
+
+    // Build social object and add to profileFields
+    const socialfields = { youtube, twitter, instagram, linkedin, facebook }
+    for (const [key, value] of Object.entries(socialfields)) {
+        if (value && value.length > 0) socialfields[key] = normalize(value, { forceHttps: true })
+    }
+
+    // set the values of socialfields to profileFields.social
+    profileFields.social = socialfields
+
+    try {
+        // Using upsert option (create new doc if no match is found)
+        let profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true, upsert: true })
+        return res.json(profile)
     } catch (err) {
         console.error(err.message)
         res.status(500).send('Server Error')
